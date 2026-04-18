f1c0e1365c669911f0886154c6207b066aa2caa2
Alisha Hasan Chowdhury <alisha.chowdhury@ufl.edu>
2026-04-18 16:51:35 -0400

    Updated UI of "Add Courses" page.

--- MERN/client/tailwind.config.js
+++ MERN/client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cream': '#F2EAE0',
+        'light-blue': '#B4D3D9',
+        'light-purple': '#BDA6CE',
+        'dark-purple': '#9B8EC7',
+      },
+    },
   },
   plugins: [],
 }
